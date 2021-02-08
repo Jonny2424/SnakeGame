34509af29f379d32a5888f7d9336ca3797da187c
Vaflor Jonathan <VaflorJonathan@JohnDeere.com>
2021-02-08 14:12:56 -0600

    added game.js, snake.js, and input.js

---
+++ game.js
@@ -0,0 +1,28 @@
+import {snakeSpeed, update as updateSnake, draw as drawSnake} from './snake.js';
+
+let lastRenderTime = 0;
+const gameBoard = document.getElementById('game-board');
+
+
+function main(currentTime) {
+    window.requestAnimationFrame(main)
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+    if (secondsSinceLastRender < 1/snakeSpeed) return
+    console.log("Render")
+    lastRenderTime = currentTime;
+
+
+    update()
+    draw()
+}
+
+window.requestAnimationFrame(main)
+
+function update() {
+    updateSnake()
+}
+
+function draw() {
+    gameBoard.innerHTML = '';
+    drawSnake(gameBoard)
+}
